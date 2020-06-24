120511b36bac4965d9c89e7718fdfa287e8d195b
nagavineerpasam <nagavineerpasam@gmail.com>
2020-06-24 12:16:46 +0530

    Updates

---
+++ precache-manifest.7d707360cca3934758d6179822681cb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "104791da5e78f6224050f4b61c9e39c9",
+    "url": "/moviesearchApp/index.html"
+  },
+  {
+    "revision": "f22d026dae90b5cc5033",
+    "url": "/moviesearchApp/static/css/main.133f275e.chunk.css"
+  },
+  {
+    "revision": "d073294195bb06cc6a95",
+    "url": "/moviesearchApp/static/js/2.073d1c1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moviesearchApp/static/js/2.073d1c1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f22d026dae90b5cc5033",
+    "url": "/moviesearchApp/static/js/main.26ff8c23.chunk.js"
+  },
+  {
+    "revision": "ec2fdc3c167c1e6de341",
+    "url": "/moviesearchApp/static/js/runtime-main.cd6c24b7.js"
+  }
+]);
