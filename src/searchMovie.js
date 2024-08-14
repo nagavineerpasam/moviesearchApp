da50cf35b944c92f92834c72298702a2f92680a6
vineerpasam <60692436+nagavineerpasam@users.noreply.github.com>
2024-08-14 12:52:02 +0530

    Update searchMovie.js

--- src/searchMovie.js
+++ src/searchMovie.js
@@ -10,7 +10,7 @@ export default function SearchMovies(){
     const searchMovies = async (e) => {
         e.preventDefault();
                 
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+        const url = `https://api.themoviedb.org/3/search/movie?api_key={apiKey}&language=en-US&query=${query}&page=1&include_adult=false`;
         
         try {
             const res = await fetch(url);
